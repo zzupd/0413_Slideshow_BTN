bcd80f01923490fbcf75866234b9a9ff4f962ded
zzupd <zzupd@naver.com>
2021-04-13 13:20:44 +0900

    0413_Next버튼 기능구현완료

--- script/script.js
+++ script/script.js
@@ -1,5 +1,21 @@
 $(function(){
 
+    //1. 정방향 슬라이드 적용( 버튼기호 ">", Next )
+
+    $("#btnNext>span").click(function(){
+
+        $("#shuttleFrame").animate({
+            "margin-left": "-100px"
+            },
+            800,
+            function(){
+
+                $("#shuttleFrame").css({"margin-left": "0"});
+                $(".slideBox:first-child")
+                    .insertAfter(".slideBox:last-child");
+            });
+
+    });
 
 
 });
