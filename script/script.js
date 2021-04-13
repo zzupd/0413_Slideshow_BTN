24c6f415c26bd9a15d16d00d13feaa605ab09e69
zzupd <zzupd@naver.com>
2021-04-13 14:05:53 +0900

    0413_슬라이드쇼_버튼적용_작업완료

--- script/script.js
+++ script/script.js
@@ -1,21 +1,36 @@
 $(function(){
 
     //1. 정방향 슬라이드 적용( 버튼기호 ">", Next )
-
     $("#btnNext>span").click(function(){
-
         $("#shuttleFrame").animate({
             "margin-left": "-100px"
             },
             800,
             function(){
-
                 $("#shuttleFrame").css({"margin-left": "0"});
                 $(".slideBox:first-child")
                     .insertAfter(".slideBox:last-child");
             });
+    });
+
+    //2. 역방향 슬라이드 적용( 버튼기호 "<", Prev)
+    $("#btnPrev>span").click(function(){
+
+        $(".slideBox:last-child")
+                    .insertBefore(".slideBox:first-child");
+        $("#shuttleFrame").css({"margin-left": "-100px"});
+
+        $("#shuttleFrame").animate({
+            "margin-left": "0px"
+            },
+            800);
 
     });
+
+
+
+
+
 
 
 });
